8f9f63943a06d1af4e3e2eefac2a5aca1565bf97
Elie Bursztein <github@elie.net>
2020-05-03 16:55:53 -0700

    Refactored uniform crossover fix #5

--- geneflow/ops/uniform_crossover.py
+++ geneflow/ops/uniform_crossover.py
@@ -15,213 +15,192 @@
 from termcolor import cprint
 from geneflow.engine import OP
 from geneflow import backend as B
-from tabulate import tabulate
 
 
 class UniformCrossover(OP):
-
-    def __init__(self, num_crossover_fraction=0.9,
-                 crossover_size_fraction=0.2, **kwargs):
+    def __init__(self, population_fraction, crossover_probability, **kwargs):
         """Perform uniform crossovers on a given population.
 
         Args:
-            num_crossover_fraction (float, optional): How many chromosomes
-            should have a cross-over. Defaults to 0.9 (90%).
-            crossover_size_fraction (float, optional): What fraction of the
-            gene should be affected by the crossover. Defaults to 0.2 (20%).
-            debug (bool, optional): [description]. Defaults to False.
+            population_fraction (float): How many chromosomes
+            should have a cross-over.
+            crossover_probability (list(float)): What fraction of the
+            gene should be affected by crossovers.
+            debug (bool, optional): print debug information and function
+            returns additional data.
+        Returns:
+            tensor: population with a crossover.
         """
 
-        if not (0 < num_crossover_fraction <= 1.0):
-            raise ValueError("num_crossover_fraction must be in ]0. 1]")
+        if not (0 < population_fraction <= 1.0):
+            raise ValueError("population_fraction must be in ]0. 1]")
 
-        for val in crossover_size_fraction:
-            if not (0 < num_crossover_fraction <= 1.0):
+        for val in crossover_probability:
+            if not (0 < val <= 1.0):
                 raise ValueError(
-                    "values in crossover_size_fraction must be between ]0. 1]")
+                    "values in crossover_probability must be between ]0. 1]")
 
-        self.num_crossover_fraction = num_crossover_fraction
-        self.crossover_size_fraction = crossover_size_fraction
+        self.population_fraction = population_fraction
+        self.crossover_probability = crossover_probability
         self.x_matrix = None
         self.has_cache = False
         self.mutation_shape = []
         super(UniformCrossover, self).__init__(**kwargs)
 
-    def call(self, chromosome_populations):
+    def call(self, populations):
         results = []
-        for chromosomes in chromosome_populations:
-            results.append(self.compute(chromosomes))
+        for population in populations:
+            results.append(self.compute(population))
         return results
 
-    def compute(self, chromosomes):
+    def compute(self, population):
 
         # mix genomes
-        B.shuffle(chromosomes)
-        chromosomes2 = B.copy(chromosomes)
-        B.shuffle(chromosomes2)
+        population_copy = B.copy(population)
+        B.shuffle(population_copy)
 
-        # replace genome
+        # save population for debug
         if self.debug:
-            ref_chromosomes = B.copy(chromosomes)
+            original_population = B.copy(population)
 
         # how many chromosomes to crossover?
-        num_crossover_chromosomes = int(chromosomes.shape[0] *
-                                        self.num_crossover_fraction)
-        self.print_debug("num_crossover_chromosome %s" %
-                         num_crossover_chromosomes)
+        num_crossovers = int(population.shape[0] * self.population_fraction)
+        self.print_debug("population size %s" % population.shape[0])
+        self.print_debug("num_crossovers %s" % num_crossovers)
 
         # crossover matrix
         if not self.has_cache:
             self.print_debug("Creating x_matrix and mutation_matrix")
             self.has_cache = True
-            self.x_matrix = B.zeros(chromosomes.shape, dtype='int')
+            self.x_matrix = B.zeros(population.shape, dtype='int')
 
             # We need to accounting for the fact that the population
             # can be of rank N which makes the fancy indexing painful.
             # we need a shape for the mutation which is this:
             # [num_crossover, num_mutation, ..., num_mutations]
-            mutations_shape = [num_crossover_chromosomes]
-            for idx, frac in enumerate(self.crossover_size_fraction):
+            mutations_shape = [num_crossovers]
+            for idx, frac in enumerate(self.crossover_probability):
                 num_genes = int(self.x_matrix.shape[idx + 1] * frac)
                 mutations_shape.append(num_genes)
             self.print_debug("mutation_shape: %s" % mutations_shape)
             self.mutation_shape = mutations_shape
 
-            # mutations_shape = B.tensor(mutations_shape, dtype='int')
+            # create tensor
             mutations = B.ones(mutations_shape)
 
             # compute the fancy indexing dynamically
-            slices = [slice(0, num_crossover_chromosomes, 1)]
-            for size in mutations_shape[1:]:
-                slices.append(slice(0, size, 1))
+            slices = []
+            for size in mutations_shape:
+                slices.append(slice(0, size))
             slices = tuple(slices)
 
             # injecting mutations
             self.x_matrix[slices] = mutations
-
-            # shuffling preserve structure. Here we need to shuffle
-            # all chromsomes dimensions so we will flatten the matrix
-            # and shuffle its transpose before recreating it matrix
-            total_chromosome_size = B.prod(B.tensor(self.x_matrix.shape[1:]))
-            flatten_shape = (self.x_matrix.shape[0],
-                             int(total_chromosome_size))
-            self.flatten_x_matrix = B.reshape(self.x_matrix, flatten_shape)
         else:
-            self.print_debug("Using cache")
+            self.print_debug("Using cached matrix")
 
-        B.shuffle(self.flatten_x_matrix)
-        B.shuffle(self.flatten_x_matrix.T)
-
-        self.x_matrix = B.reshape(self.flatten_x_matrix, self.x_matrix.shape)
+        B.full_shuffle(self.x_matrix)
 
         # invert crossover matrix
         inv_x_matrix = B.abs((self.x_matrix) - 1)
 
         # copy chromosomes that stays the same
-        mutated_chromosomes = chromosomes * inv_x_matrix
+        population = population * inv_x_matrix
 
         # add the mutations
-        mutated_chromosomes += (chromosomes2 * self.x_matrix)
+        population += (population_copy * self.x_matrix)
 
         if self.debug:
-            return {'mutated': mutated_chromosomes,
-                    'original': ref_chromosomes,
-                    'num_crossover_chromosome': num_crossover_chromosomes,
+            return {
+                'mutated': population,
+                'original': original_population,
+                'num_crossover_chromosome': num_crossovers,
+            }
 
-                    }
-
-        return mutated_chromosomes
+        return population
 
 
 class UniformCrossover1D(UniformCrossover):
+    def __init__(self,
+                 population_fraction=0.9,
+                 crossover_probability=0.2,
+                 **kwargs):
+        if not isinstance(crossover_probability, float):
+            raise ValueError('crossover_probability must be a float')
 
-    def __init__(self, num_crossover_fraction=0.9,
-                 crossover_size_fraction=0.2, **kwargs):
-
-        super(UniformCrossover1D, self).__init__(
-            num_crossover_fraction=num_crossover_fraction,
-            crossover_size_fraction=[crossover_size_fraction],
-            **kwargs)
+        super(UniformCrossover1D,
+              self).__init__(population_fraction=population_fraction,
+                             crossover_probability=[crossover_probability],
+                             **kwargs)
 
 
 class UniformCrossover2D(UniformCrossover):
+    def __init__(self,
+                 population_fraction=0.9,
+                 crossover_probability=(0.2, 0.2),
+                 **kwargs):
 
-    def __init__(self, num_crossover_fraction=0.9,
-                 crossover_size_fraction=(0.2, 0.2), **kwargs):
-
-        super(UniformCrossover2D, self).__init__(
-            num_crossover_fraction=num_crossover_fraction,
-            crossover_size_fraction=crossover_size_fraction,
-            **kwargs)
+        if len(crossover_probability) != 2:
+            raise ValueError('crossover_size_fraction must be of form (x, y)')
+        super(UniformCrossover2D,
+              self).__init__(population_fraction=population_fraction,
+                             crossover_probability=crossover_probability,
+                             **kwargs)
 
 
 class UniformCrossover3D(UniformCrossover):
-
-    def __init__(self, num_crossover_fraction=0.9,
-                 crossover_size_fraction=(0.2, 0.2, 0.2), **kwargs):
-
-        super(UniformCrossover3D, self).__init__(
-            num_crossover_fraction=num_crossover_fraction,
-            crossover_size_fraction=crossover_size_fraction,
-            **kwargs)
+    def __init__(self,
+                 population_fraction=0.9,
+                 crossover_probability=(0.2, 0.2, 0.2),
+                 **kwargs):
+        if len(crossover_probability) != 3:
+            raise ValueError(
+                'crossover_probability must be of form (x, y, z)')  # noqa
+        super(UniformCrossover3D,
+              self).__init__(population_fraction=population_fraction,
+                             crossover_probability=crossover_probability,
+                             **kwargs)
 
 
 if __name__ == '__main__':
+    from copy import copy
     print(B.backend())
     GENOME_SHAPE = (10, 4, 4)
     chromosomes = B.randint(0, 1024, GENOME_SHAPE)
-    num_crossover_fraction = 0.5
-    max_crossover_size_fraction = (0.5, 0.5)
+    population_fraction = 0.5
+    crossover_probability = (0.5, 0.5)
 
     print(chromosomes.shape)
     # peforming crossover
-    res = UniformCrossover2D(num_crossover_fraction,
-                             max_crossover_size_fraction,
+    res = UniformCrossover2D(population_fraction,
+                             crossover_probability,
                              debug=True)(chromosomes)
 
     # diff matrix
     diff = B.clip(abs(res['mutated'] - res['original']), 0, 1)
-
+    print(diff)
     # expected mutated chromosomes
-    expected_mutated = chromosomes.shape[0] * num_crossover_fraction
-    cprint("Expected mutated chromosome:%d (+/- 1 due to collision)" %
-           (expected_mutated), 'cyan')
+    expected_mutated = chromosomes.shape[0] * population_fraction
+    cprint(
+        "Expected mutated chromosome:%d (+/- 1 due to collision)" %
+        (expected_mutated), 'cyan')
     # select mutated chromosomes
     mutated_chromosomes = []
     for c in diff:
         if B.max(c):
             mutated_chromosomes.append(c)
-    cprint(mutated_chromosomes[0], 'cyan')
-
     cprint("mutated chromosome:%d" % (len(mutated_chromosomes)), 'magenta')
 
-    bins = B.bincount(diff.flatten())
-    mutation_rate = int(bins[1] / sum(bins) * 100)
+    cprint("[example of mutated chromosome]", 'yellow')
+    cprint(mutated_chromosomes[0], 'cyan')
 
-    # expected mutated rows
-    mutated_rows = max_crossover_size_fraction[0] * GENOME_SHAPE[1]
-    cprint('expected mutated row:%d' % mutated_rows, 'cyan')
-    mutated_cells = max_crossover_size_fraction[0] * GENOME_SHAPE[2]
-    cprint('expected mutated cell:%d' % mutated_cells, 'cyan')
-
-    # actual mutated rows:
-    rows = []
-    for cidx, c in enumerate(mutated_chromosomes):
-        mr = 0
-        for r in c:
-            mc = B.sum(r)
-            if mc:
-                mr += 1
-        rows.append([cidx, mr, mc])
-    print(tabulate(rows, headers=["Chromosome", "Row mutated",
-                                  "Cell per row mutated"]))
-
+    OP = UniformCrossover2D(population_fraction, crossover_probability)
+    res = OP(chromosomes)
     for _ in range(100):
-        res = UniformCrossover2D(num_crossover_fraction,
-                                 max_crossover_size_fraction,
-                                 debug=1)(chromosomes)
+        prev = copy(res)
+        res = OP(chromosomes)
         # acumulating diff matrix
-        diff += B.clip(abs(res['mutated'] - res['original']), 0, 1)
+        diff += B.clip(abs(res - prev), 0, 1)
 
-    cprint("[cumulative diff matrix]", 'yellow')
+    cprint("[cumulative diff matrix 100 runs]", 'yellow')
     cprint(diff[0], 'cyan')
--- tests/ops/test_uniform_crossover.py
+++ tests/ops/test_uniform_crossover.py
@@ -1,45 +1,48 @@
-from termcolor import cprint
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import geneflow.backend as B
+from copy import copy
 from geneflow.ops import UniformCrossover1D, UniformCrossover2D
 from geneflow.ops import UniformCrossover3D
 
 
-def test_crossover2d_shape():
-    GENOME_SHAPE = (10, 4, 4)
-    MAX_VAL = 250
-    chromosomes = B.randint(0, MAX_VAL, GENOME_SHAPE)
-    num_crossover_fraction = 0.5
-    max_crossover_size_fraction = (0.5, 0.5)
-
-    res = UniformCrossover2D(num_crossover_fraction,
-                             max_crossover_size_fraction)(chromosomes)
-    assert res.shape == GENOME_SHAPE
-    assert B.max(res) <= MAX_VAL
-
-
 def test_uniform_2Dcrossover_randomness_shape():
     GENOME_SHAPE = (10, 4, 4)
-    chromosomes = B.randint(0, 1024, GENOME_SHAPE)
-    num_crossover_fraction = 0.5
-    max_crossover_size_fraction = (0.5, 0.5)
+    population = B.randint(0, 1024, GENOME_SHAPE)
+    population_fraction = 0.5
+    crossover_probability = (0.5, 0.5)
 
-    res = UniformCrossover2D(num_crossover_fraction,
-                             max_crossover_size_fraction,
-                             debug=True)(chromosomes)
+    original_population = copy(population)
+    OP = UniformCrossover2D(population_fraction, crossover_probability)
+    population = OP(population)
 
-    diff = B.clip(abs(res['mutated'] - res['original']), 0, 1)
-    expected_mutated = chromosomes.shape[0] * num_crossover_fraction
+    diff = B.clip(abs(population - original_population), 0, 1)
+    print(diff)
+    expected_mutations = original_population.shape[0] * population_fraction
 
     mutated_chromosomes = []
     for c in diff:
         if B.max(c):
             mutated_chromosomes.append(c)
+    num_mutations = len(mutated_chromosomes)
 
-    # sometime we have a collision
-    assert abs(len(mutated_chromosomes) - expected_mutated) < 2
+    # sometime we have a collision so we use a delta
+    assert abs(num_mutations - expected_mutations) < 2
 
-    mutated_rows = max_crossover_size_fraction[0] * GENOME_SHAPE[1]
-    mutated_cells = max_crossover_size_fraction[0] * GENOME_SHAPE[2]
+    mutated_rows = crossover_probability[0] * GENOME_SHAPE[1]
+    mutated_cells = crossover_probability[0] * GENOME_SHAPE[2]
     for cidx, c in enumerate(mutated_chromosomes):
         mr = 0
         mc = 0
@@ -54,32 +57,75 @@ def test_uniform_2Dcrossover_randomness_shape():
 
 
 def test_uniform_2Dcrossover_distribution():
-    "check that every gene of the tensor are going to be flipped"
-    GENOME_SHAPE = (10, 4, 4)
-    chromosomes = B.randint(0, 1024, GENOME_SHAPE)
-    num_crossover_fraction = 0.5
-    max_crossover_size_fraction = (0.5, 0.5)
+    """check that every gene of the tensor are going to be flipped
+    Note:
+    # ! We need enough iterations and chromosomes to reduce collision
+    # ! and ensure numerical stability
+    """
+    NUM_ITERATIONS = 1000
+    GENOME_SHAPE = (20, 4, 4)
+    population = B.randint(0, 1024, GENOME_SHAPE)
+    population_fraction = 1
+    crossover_probability = (0.5, 0.5)
 
-    res = UniformCrossover2D(num_crossover_fraction,
-                             max_crossover_size_fraction,
-                             debug=True)(chromosomes)
+    # each gene proba of being mutated 0.5*0.5 > 0.25
+    # each chromosome proba of being mutated 1
+    # => gene average hit rate: 1000 / (1/4)  ~250
+    MIN_DIFF_BOUND = 200
+    MAX_DIFF_BOUND = 300
+
+    OP = UniformCrossover2D(population_fraction, crossover_probability)
 
     # diff matrix
-    UC = UniformCrossover2D(num_crossover_fraction,
-                            max_crossover_size_fraction,
-                            debug=1)
-    diff = B.clip(abs(res['mutated'] - res['original']), 0, 1)
-    for _ in range(200):
-        res = UC(chromosomes)
-        # acumulating diff matrix
-        diff += B.clip(abs(res['mutated'] - res['original']), 0, 1)
+    previous_population = copy(population)
+    population = OP(population)
+    diff = B.clip(abs(population - previous_population), 0, 1)
+    for _ in range(NUM_ITERATIONS - 1):
+        previous_population = copy(population)
+        population = OP(population)
 
-    # each gene proba of being mutated 1/2*1/2 > 1/4
-    # each chromosome proba of being mutated 1/2
-    # => gene average hit rate: 1000 / (1/4*1/2)  ~25
+        curr_diff = B.clip(abs(population - previous_population), 0, 1)
+        # acumulating diff matrix
+        diff += curr_diff
+
+    print(curr_diff)
+
     for c in diff:
         print(c)
         print('mean', B.mean(c), 'min', B.min(c), 'max', B.max(c))
-        assert B.min(c) > 10
-        assert B.max(c) < 50
-        assert 10 < B.mean(c) < 50
+        assert B.min(c) > MIN_DIFF_BOUND
+        assert B.max(c) < MAX_DIFF_BOUND
+        assert MIN_DIFF_BOUND < B.mean(c) < MAX_DIFF_BOUND
+
+
+def test_ND():
+    "test various tensor size random"
+
+    TEST_INPUTS = [
+        [UniformCrossover1D, (2, 4), 0.5],
+        [UniformCrossover2D, (2, 4, 4), (0.5, 0.5)],
+        [UniformCrossover3D, (2, 4, 4, 4), (0.5, 0.5, 0.5)],
+    ]
+
+    for inputs in TEST_INPUTS:
+        OP = inputs[0]
+        pop_shape = inputs[1]
+        mutations_probability = inputs[2]
+        population_fraction = 1
+        population = B.randint(0, 1024, pop_shape)
+
+        # eager
+        RM = OP(population_fraction=population_fraction,
+                mutations_probability=mutations_probability)
+
+        population = RM(population)
+        assert B.is_tensor(population)
+        assert population.shape == pop_shape
+
+        # graph
+        RM = OP(population_fraction=population_fraction,
+                mutations_probability=mutations_probability)
+
+        population = RM._call_from_graph(population)
+        assert B.is_tensor(population)
+        assert population.shape == pop_shape
