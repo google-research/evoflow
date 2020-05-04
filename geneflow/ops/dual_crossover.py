e589d1e9afe31ba245f52a9a9bb477bccf7f7ae5
Elie Bursztein <github@elie.net>
2020-05-03 17:32:17 -0700

    Dual crossover refactored fix #6

---
+++ geneflow/ops/dual_crossover.py
@@ -0,0 +1,182 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+from geneflow.engine import OP
+from geneflow import backend as B
+
+
+class DualCrossover(OP):
+    def __init__(self, population_fraction, crossover_probability,
+                 **kwargs):
+        """Perform Dual crossovers on a given population.
+
+        Args:
+            population_fraction (float): How many chromosomes
+            should have a cross-over.
+
+            crossover_probability (list(float)): What fraction of the
+            gene should be affected by crossovers.
+
+            debug (bool, optional): print debug information and function
+            returns additional data.
+
+        Returns:
+            tensor: population with a crossover.
+
+        See:
+            https://en.wikipedia.org/wiki/Crossover_(genetic_algorithm)
+        """
+
+        if not (0 < population_fraction <= 1.0):
+            raise ValueError("population_fraction must be in ]0. 1]")
+
+        for val in crossover_probability:
+            if not (0 < val <= 1.0):
+                raise ValueError(
+                    "crossover_size_fraction values must be between ]0. 1]")
+
+        self.population_fraction = population_fraction
+        self.crossover_probability = crossover_probability
+        self.has_cache = False
+        self.mutation_shape = []
+        self.slices = None
+        super(DualCrossover, self).__init__(**kwargs)
+
+    def call(self, populations):
+        results = []
+        for population in populations:
+            results.append(self.compute(population))
+        return results
+
+    def compute(self, population):
+
+        # mix genomes
+        B.shuffle(population)
+        population_copy = B.copy(population)
+        B.shuffle(population_copy)
+
+        # save population for debug
+        if self.debug:
+            original_population = B.copy(population)
+
+        # how many chromosomes to crossover
+        num_crossover_chromosomes = int(population.shape[0] *
+                                        self.population_fraction)
+
+        if self.debug:
+            print('num chromosomes', num_crossover_chromosomes)
+
+        if not self.has_cache:
+            self.print_debug("Caching fancy indexing")
+            self.has_cache = True
+
+            # compute the shape needed for the mutation
+            mutations_shape = [num_crossover_chromosomes]
+            for idx, frac in enumerate(self.crossover_probability):
+                num_genes = int(population.shape[idx + 1] * frac)
+                mutations_shape.append(num_genes)
+            self.mutations_shape = mutations_shape
+            self.print_debug("mutation_shape: %s" % self.mutations_shape)
+
+        else:
+            self.print_debug("Using cached fancy indexing")
+
+        # compute the fancy indexing dynamlically
+        # ! the start point must be randomized
+        slices = [slice(0, num_crossover_chromosomes)]
+        for idx, crossover_size in enumerate(self.mutations_shape[1:]):
+            # ! making indexing explicit as its a huge pitfall
+            mutation_dim = idx + 1
+            max_start = population.shape[mutation_dim] - crossover_size
+
+            # Using python random as we only need a single integer
+            start = random.randint(0, max_start)
+
+            slices.append(slice(start, crossover_size + start))
+
+        slices = tuple(slices)
+        self.print_debug(slices)
+
+        # crossover
+        cross_section = population_copy[slices]
+        population[slices] = cross_section
+
+        if self.debug:
+            return {
+                'mutated': population,
+                'original': original_population,
+                'num_crossover_chromosome': num_crossover_chromosomes,
+            }
+        return population
+
+
+class DualCrossover1D(DualCrossover):
+    def __init__(self,
+                 population_fraction=0.9,
+                 crossover_probability=0.2,
+                 **kwargs):
+        if not isinstance(crossover_probability, float):
+            raise ValueError('crossover_probability must be a float')
+
+        super(DualCrossover1D,
+              self).__init__(population_fraction=population_fraction,
+                             crossover_probability=[crossover_probability],
+                             **kwargs)
+
+
+class DualCrossover2D(DualCrossover):
+    def __init__(self,
+                 population_fraction=0.9,
+                 crossover_probability=(0.2, 0.2),
+                 **kwargs):
+
+        if len(crossover_probability) != 2:
+            raise ValueError('crossover_probability must be of form (x, y)')
+        super(DualCrossover2D,
+              self).__init__(population_fraction=population_fraction,
+                             crossover_probability=crossover_probability,
+                             **kwargs)
+
+
+class DualCrossover3D(DualCrossover):
+    def __init__(self,
+                 population_fraction=0.9,
+                 crossover_probability=(0.2, 0.2, 0.2),
+                 **kwargs):
+
+        if len(crossover_probability) != 3:
+            raise ValueError('crossover_probability must be of form (x, y, z)')
+
+        super(DualCrossover3D,
+              self).__init__(population_fraction=population_fraction,
+                             crossover_probability=crossover_probability,
+                             **kwargs)
+
+
+if __name__ == '__main__':
+    print(B.backend())
+    GENOME_SHAPE = (6, 4, 4)
+    chromosomes = B.randint(0, 256, GENOME_SHAPE)
+    population_fraction = 0.5
+    max_crossover_size_fraction = (0.5, 0.5)
+    print(chromosomes.shape)
+
+    res = DualCrossover2D(population_fraction,
+                          max_crossover_size_fraction,
+                          debug=True)(chromosomes)
+
+    # diff matrix
+    diff = B.clip(abs(res['mutated'] - res['original']), 0, 1)
+    print(diff)
---
+++ tests/ops/test_dual_crossover.py
@@ -0,0 +1,113 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# import os
+# os.environ['GENEFLOW_BACKEND'] = 'numpy'
+from copy import copy
+from geneflow import backend as B
+from geneflow.ops import DualCrossover1D, DualCrossover2D
+from geneflow.ops import DualCrossover3D
+
+
+def test_ND():
+    "test various tensor size random"
+
+    TEST_INPUTS = [
+        [DualCrossover1D, (2, 4), 0.5],
+        [DualCrossover2D, (2, 4, 4), (0.5, 0.5)],
+        [DualCrossover3D, (2, 4, 4, 4), (0.5, 0.5, 0.5)],
+    ]
+
+    for inputs in TEST_INPUTS:
+        OP = inputs[0]
+        pop_shape = inputs[1]
+        mutations_probability = inputs[2]
+        population_fraction = 1
+        population = B.randint(0, 1024, pop_shape)
+
+        # eager
+        RM = OP(population_fraction=population_fraction,
+                mutations_probability=mutations_probability)
+
+        population = RM(population)
+        assert B.is_tensor(population)
+        assert population.shape == pop_shape
+
+        # graph
+        RM = OP(population_fraction=population_fraction,
+                mutations_probability=mutations_probability)
+
+        population = RM._call_from_graph(population)
+        assert B.is_tensor(population)
+        assert population.shape == pop_shape
+
+
+def test_crossover1D_output_shape():
+    POPULATION_SHAPE = (64, 128)
+    population = B.randint(0, 1024, POPULATION_SHAPE)
+    population_fraction = 0.5
+    mutations_probability = 0.2
+
+    res = DualCrossover1D(population_fraction,
+                          mutations_probability,
+                          debug=True)(population)
+
+    # measuring mutation rate
+    diff = B.clip(B.abs(res['original'] - res['mutated']), 0, 1)
+    # cprint(diff, 'cyan')
+
+    # row test
+    num_ones_in_row = 0
+    for col in diff:
+        num_ones_in_row = max(list(col).count(1), num_ones_in_row)
+
+    max_one_in_row = int(POPULATION_SHAPE[1] * mutations_probability)
+    assert num_ones_in_row == max_one_in_row
+    assert num_ones_in_row
+
+
+def test_dualcrossover2d_distribution():
+    """check that every gene of the tensor are going to be flipped equally
+    Note:
+    # ! We need enough iterations and chromosomes to reduce collision
+    # ! and ensure numerical stability
+    """
+    NUM_ITERATIONS = 1000
+    GENOME_SHAPE = (20, 4, 4)
+    population = B.randint(0, 1024, GENOME_SHAPE)
+    population_fraction = 1
+    crossover_probability = (0.5, 0.5)
+
+    OP = DualCrossover2D(population_fraction, crossover_probability)
+
+    # diff matrix
+    previous_population = copy(population)
+    population = OP(population)
+    diff = B.clip(abs(population - previous_population), 0, 1)
+    for _ in range(NUM_ITERATIONS - 1):
+        previous_population = copy(population)
+        population = OP(population)
+
+        curr_diff = B.clip(abs(population - previous_population), 0, 1)
+        # acumulating diff matrix
+        diff += curr_diff
+
+    print(curr_diff)
+
+    for c in diff:
+        print(c)
+        print('mean', B.mean(c), 'min', B.min(c), 'max', B.max(c))
+        assert B.min(c) > NUM_ITERATIONS * 0.9
+        assert B.max(c) < NUM_ITERATIONS
+        assert NUM_ITERATIONS * 0.9 < B.mean(c) < NUM_ITERATIONS
